db46e1748f9398d3e7e59f57703271d49eb34b77
XiaNiCai <zhanglibinit@163.com>
2017-06-22 18:04:16 +0800

    修改：添加弹幕功能

--- app/src/main/java/com/example/lenovo/kuaikan/home/comicread/view/ReadActivity.java
+++ app/src/main/java/com/example/lenovo/kuaikan/home/comicread/view/ReadActivity.java
@@ -2,11 +2,13 @@ package com.example.lenovo.kuaikan.home.comicread.view;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,9 +24,18 @@ import com.lsjwzh.widget.materialloadingprogressbar.CircleProgressBar;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.OnClick;
+import master.flame.danmaku.controller.DrawHandler;
+import master.flame.danmaku.danmaku.model.BaseDanmaku;
+import master.flame.danmaku.danmaku.model.DanmakuTimer;
+import master.flame.danmaku.danmaku.model.IDanmakus;
+import master.flame.danmaku.danmaku.model.android.DanmakuContext;
+import master.flame.danmaku.danmaku.model.android.Danmakus;
+import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
+import master.flame.danmaku.ui.widget.DanmakuView;
 
 public class ReadActivity extends BaseActivity implements IReadView {
 
@@ -43,6 +54,10 @@ public class ReadActivity extends BaseActivity implements IReadView {
     ConstraintLayout mLayoutComment;
     @BindView(R.id.progressBar)
     CircleProgressBar mProgressBar;
+    @BindView(R.id.ed_comment)
+    EditText mEdComment;
+    @BindView(R.id.danmaku_view)
+    DanmakuView mDanmakuView;
     private ReadPresenterImpl mReadPresenter;
     private List<String> mImages;
     private ReadAdapter mReadAdapter;
@@ -50,6 +65,17 @@ public class ReadActivity extends BaseActivity implements IReadView {
     private List<BeanRead.DataBean.ImageInfosBean> mImageInfos;
     private String comicId;
     private List<BeanComments.DataBean.CommentsBean> mComments;
+    //弹幕
+    private boolean showDanmaku;
+
+    private DanmakuContext danmakuContext;
+
+    private BaseDanmakuParser parser = new BaseDanmakuParser() {
+        @Override
+        protected IDanmakus parse() {
+            return new Danmakus();
+        }
+    };
 
     @Override
     public int getlayoutId() {
@@ -81,6 +107,34 @@ public class ReadActivity extends BaseActivity implements IReadView {
 
             }
         });
+
+        //弹幕
+        mDanmakuView.enableDanmakuDrawingCache(true);
+        mDanmakuView.setCallback(new DrawHandler.Callback() {
+            @Override
+            public void prepared() {
+                showDanmaku = true;
+                mDanmakuView.start();
+                generateSomeDanmaku();
+            }
+
+            @Override
+            public void updateTimer(DanmakuTimer timer) {
+
+            }
+
+            @Override
+            public void danmakuShown(BaseDanmaku danmaku) {
+
+            }
+
+            @Override
+            public void drawingFinished() {
+
+            }
+        });
+        danmakuContext = DanmakuContext.create();
+        mDanmakuView.prepare(parser, danmakuContext);
     }
 
 
@@ -153,4 +207,53 @@ public class ReadActivity extends BaseActivity implements IReadView {
     public void onViewClicked() {
     }
 
+
+    /**
+     * 向弹幕View中添加一条弹幕
+     * @param content
+     *          弹幕的具体内容
+     * @param  withBorder
+     *          弹幕是否有边框
+     */
+    private void addDanmaku(String content, boolean withBorder) {
+        BaseDanmaku danmaku = danmakuContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
+        danmaku.text = content;
+        danmaku.padding = 5;
+        danmaku.textSize = sp2px(20);
+        danmaku.textColor = Color.WHITE;
+        danmaku.setTime(mDanmakuView.getCurrentTime());
+        if (withBorder) {
+            danmaku.borderColor = Color.GREEN;
+        }
+        mDanmakuView.addDanmaku(danmaku);
+    }
+
+    /**
+     * 随机生成一些弹幕内容以供测试
+     */
+    private void generateSomeDanmaku() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(showDanmaku) {
+                    int time = new Random().nextInt(300);
+                    String content = "" + time + time;
+                    addDanmaku(content, false);
+                    try {
+                        Thread.sleep(time);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * sp转px的方法。
+     */
+    public int sp2px(float spValue) {
+        final float fontScale = getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
 }
